eccbdbb3de62774162c792d3fdd9fa045c62c424
John McBride <john@johnmcbride.me>
2017-10-25 00:24:04 -0400

    Added sdk docs node and root node items for treeview

---
+++ src/Model/SDKDocs/SDKDocument.ts
@@ -0,0 +1,12 @@
+'use strict';
+import * as vscode from 'vscode';
+
+export class SDKDocument
+{
+    constructor( public readonly label: string,
+                 public readonly link: string   ) {
+        //super(label);
+        
+    }
+
+}
