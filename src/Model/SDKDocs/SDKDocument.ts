6771f562c157486f682ceaedd6eb41f97316eeaf
John McBride <john@johnmcbride.me>
2018-05-24 09:15:09 -0400

    Added SDKDocument class

--- src/Model/SDKDocs/SDKDocument.ts
+++ src/Model/SDKDocs/SDKDocument.ts
@@ -1,12 +1,12 @@
-'use strict';
-import * as vscode from 'vscode';
-import { SDKDocumentSubPage } from './SDKDocumentSubPage';
-
 export class SDKDocument
 {
-    constructor( public readonly label: string,
-                 public readonly link: string,
-                public readonly subpages: Array<SDKDocumentSubPage>) {
+    constructor
+    (
+        public title: string,
+        public url:string,
+        public children: Array<SDKDocument>
+    ) 
+    {
+        
     }
-
 }
