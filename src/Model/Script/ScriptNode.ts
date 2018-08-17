dddfca5bfc91e87c2b749e7b3c8ed59a5d29425f
John McBride <john@johnmcbride.me>
2018-08-17 12:17:14 -0400

    Added support for theme icons.

--- src/Model/Script/ScriptNode.ts
+++ src/Model/Script/ScriptNode.ts
@@ -1,10 +1,12 @@
 import * as vscode from 'vscode'
+import { Uri, ThemeIcon } from 'vscode';
 
 export class ScriptNode implements vscode.TreeItem
 {
     label: string;
     path: string;
-    iconPath?: string | vscode.Uri | { light: string | vscode.Uri; dark: string | vscode.Uri; } | vscode.ThemeIcon;
+    iconPath?: string | Uri | { light: string | Uri; dark: string | Uri } | ThemeIcon
+    resourceUri?: Uri;
     command?: vscode.Command;
     collapsibleState?: vscode.TreeItemCollapsibleState;
     contextValue?: string;
